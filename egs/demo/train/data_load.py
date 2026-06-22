2403a8e0c567a7fb048a4daa6845a84b501b1d5e
NorahFarley <furbynorah@gmail.com>
2026-06-22 01:11:30 -0400

    Modified for use in training models
    Added more parser arguments to make it easier to change filenames and filepaths

--- egs/demo/train/data_load.py
+++ egs/demo/train/data_load.py
@@ -4,27 +4,27 @@ import torch
 import random
 import numpy as np
 from tqdm import tqdm
-from utils import *
+from utils import winding_density
 
-def get_case_paths(paths):
+def get_case_paths(paths, spin_file='Spins.npy', hd_file='Hds.npy'):
     spin_paths = []
     Hd_paths = []
     for path in glob.glob(os.path.join(paths, '*')):
         try:
-            if os.path.exists((path+f'/Spins.npy')) and os.path.exists((path+f'/Hds.npy')):
-                spin_paths.append((path+f'/Spins.npy'))
-                Hd_paths.append((path+f'/Hds.npy'))
+            if os.path.exists((path+f'/{spin_file}')) and os.path.exists((path+f'/{hd_file}')):
+                spin_paths.append((path+f'/{spin_file}'))
+                Hd_paths.append((path+f'/{hd_file}'))
         except ValueError:
             print(f"Invalid path: {path}")
     return spin_paths, Hd_paths
 
-def path_split(paths, ntest, n128, ntrain, mode=None):
+def path_split(paths, ntest, n128, ntrain, spin_file, hd_file, mode=None):
     random.seed(123)
     if mode=='eval128':
         spin_paths = []
         Hd_paths = []
         for path in paths:
-            spin_path, Hd_path = get_case_paths(path)
+            spin_path, Hd_path = get_case_paths(path, spin_file, hd_file)
             spin_paths.extend(spin_path)
             Hd_paths.extend(Hd_path)
         paired_paths = list(zip(spin_paths, Hd_paths))
@@ -36,7 +36,7 @@ def path_split(paths, ntest, n128, ntrain, mode=None):
         spin_paths = []
         Hd_paths = []
         for path in paths:
-            spin_path, Hd_path = get_case_paths(path)
+            spin_path, Hd_path = get_case_paths(path, spin_file, hd_file)
             spin_paths.extend(spin_path)
             Hd_paths.extend(Hd_path)
         paired_paths = list(zip(spin_paths, Hd_paths))
@@ -80,29 +80,29 @@ def combinfilter(x_paths, y_paths, cn):
     return selected_x, selected_y
 
 
-def getdata(paths, ntest, n128, ntrain, cn, mode=None):
+def getdata(paths, ntest, n128, ntrain, cn, spin_file, hd_file, mode=None):
     if mode=='eval128':
-        x_test_paths, y_test_paths = path_split(paths, ntest, n128, ntrain, mode)
+        x_test_paths, y_test_paths = path_split(paths, ntest, n128, ntrain, spin_file, hd_file, mode)
         x_test, y_test = combinfilter(x_test_paths, y_test_paths, cn)
         return x_test, y_test
     else:
-        x_train_paths, y_train_paths, x_test_paths, y_test_paths = path_split(paths, ntest, n128, ntrain)
+        x_train_paths, y_train_paths, x_test_paths, y_test_paths = path_split(paths, ntest, n128, ntrain, spin_file, hd_file)
         x_train, y_train = combinfilter(x_train_paths, y_train_paths, cn)
         x_test, y_test = combinfilter(x_test_paths, y_test_paths, cn)
         return x_train, y_train, x_test, y_test
 
 
-def dataset_prepare(data_paths, ntest, n128, ntrain, cn, mode=None):
+def dataset_prepare(data_paths, ntest, n128, ntrain, cn, spin_file='Spins.npy', hd_file='Hds.npy', mode=None):
     print('loading data from: ', data_paths)
     if mode=='eval128':
-        X_test, Y_test = getdata(data_paths, ntest, n128, ntrain, cn, mode)
+        X_test, Y_test = getdata(data_paths, ntest, n128, ntrain, cn, spin_file, hd_file, mode)
         #prepare test set
         X_test_tensor = torch.from_numpy(X_test).float()
         y_test_tensor = torch.from_numpy(Y_test).float()
         test_dataset  = torch.utils.data.TensorDataset(X_test_tensor, y_test_tensor)
         return test_dataset
     else:
-        X_train, Y_train, X_test, Y_test = getdata(data_paths, ntest, n128, ntrain, cn)
+        X_train, Y_train, X_test, Y_test = getdata(data_paths, ntest, n128, ntrain, cn, spin_file, hd_file)
         #prepare training set
         X_train_tensor = torch.from_numpy(X_train).float()
         y_train_tensor = torch.from_numpy(Y_train).float()
