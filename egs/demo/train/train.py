52b8065f19f80c8f1eb41db09750f646c19ce797
NorahFarley <furbynorah@gmail.com>
2026-06-22 12:48:51 -0400

    Modified for past inputs

--- egs/demo/train/train.py
+++ egs/demo/train/train.py
@@ -193,7 +193,8 @@ if __name__ == '__main__':
     torch.backends.cudnn.benchmark = True
     
     # Model, optimizer, and data loaders initialization
-    model = UNet(kc=args.kc, inc=args.inch, ouc=args.inch).to(device)
+    #changed input channels to be 12 (6*2)
+    model = UNet(kc=args.kc, inc=args.inch*2, ouc=args.inch).to(device)
     optim = optimizer.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999), weight_decay=0.0001)
 
     #load data
@@ -243,14 +244,14 @@ if __name__ == '__main__':
     print('batch size 1 2 3: ',bsz1, bsz2, bsz3,'\n')
 
 
-    train_dataloader1 = torch.utils.data.DataLoader(dataset=train_dataset1, batch_size=bsz1, shuffle=True,  num_workers=8, drop_last=False)
-    train_dataloader2 = torch.utils.data.DataLoader(dataset=train_dataset2, batch_size=bsz2, shuffle=True,  num_workers=8, drop_last=False)
-    train_dataloader3 = torch.utils.data.DataLoader(dataset=train_dataset3, batch_size=bsz3, shuffle=True,  num_workers=8, drop_last=False)
+    train_dataloader1 = torch.utils.data.DataLoader(dataset=train_dataset1, batch_size=bsz1, shuffle=True,  num_workers=0, drop_last=False)
+    train_dataloader2 = torch.utils.data.DataLoader(dataset=train_dataset2, batch_size=bsz2, shuffle=True,  num_workers=0, drop_last=False)
+    train_dataloader3 = torch.utils.data.DataLoader(dataset=train_dataset3, batch_size=bsz3, shuffle=True,  num_workers=0, drop_last=False)
     
-    test_dataloader1  = torch.utils.data.DataLoader(dataset=test_dataset1,  batch_size=500, shuffle=True,  num_workers=8, drop_last=False)
-    test_dataloader2  = torch.utils.data.DataLoader(dataset=test_dataset2,  batch_size=500, shuffle=True,  num_workers=8, drop_last=False)
-    test_dataloader3  = torch.utils.data.DataLoader(dataset=test_dataset3,  batch_size=500, shuffle=True,  num_workers=8, drop_last=False)
-    test_dataloader4  = torch.utils.data.DataLoader(dataset=test_dataset4,  batch_size=500, shuffle=True,  num_workers=8, drop_last=False)
+    test_dataloader1  = torch.utils.data.DataLoader(dataset=test_dataset1,  batch_size=500, shuffle=True,  num_workers=0, drop_last=False)
+    test_dataloader2  = torch.utils.data.DataLoader(dataset=test_dataset2,  batch_size=500, shuffle=True,  num_workers=0, drop_last=False)
+    test_dataloader3  = torch.utils.data.DataLoader(dataset=test_dataset3,  batch_size=500, shuffle=True,  num_workers=0, drop_last=False)
+    test_dataloader4  = torch.utils.data.DataLoader(dataset=test_dataset4,  batch_size=500, shuffle=True,  num_workers=0, drop_last=False)
 
     # #experiment path
     # args.ckpt_path='./ex{}_bsz{}_Ir{}_Unet_kc{}_inch{}'.format(
