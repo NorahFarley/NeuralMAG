4caf139c1db8b5e2c4ce0e7922fe4c6c0600a020
NorahFarley <furbynorah@gmail.com>
2026-07-06 00:11:06 -0400

    Updates to run on cluster

--- egs/NMI/MH_evaluate/runMH.sh
+++ egs/NMI/MH_evaluate/runMH.sh
@@ -9,22 +9,22 @@ for width in 64 96
 do
     for mask in True triangle hole
     do
-        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax 0.5e-6 --Ku 0.0 --dtime 2.0e-13 --mask $mask
+        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax 0.5e-6 --Ku 0.0 --dtime 2.0e-13 --mask $mask --loss_type 'divergence' --model_name 'divergence_loss.pt'
     done
     
     for Ms in 1200 1000 800 600 400
     do
-        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms $Ms --Ax 0.5e-6 --Ku 0.0 --dtime 2.0e-13
+        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms $Ms --Ax 0.5e-6 --Ku 0.0 --dtime 2.0e-13 --loss_type 'divergence' --model_name 'divergence_loss.pt'
     done
 
     for Ku in 1e5 2e5 3e5 4e5
     do
-        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax 0.5e-6 --Ku $Ku --Kvec 1,0,0 --dtime 2.0e-13
+        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax 0.5e-6 --Ku $Ku --Kvec 1,0,0 --dtime 2.0e-13 --loss_type 'divergence' --model_name 'divergence_loss.pt'
     done
 
     for Ax in 0.7e-6 0.6e-6 0.4e-6 0.3e-6
     do
-        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax $Ax --Ku 0.0 --dtime 2.0e-13
+        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax $Ax --Ku 0.0 --dtime 2.0e-13 --loss_type 'divergence' --model_name 'divergence_loss.pt'
     done
 
 done
@@ -35,22 +35,22 @@ do
 
     for Ms in 1200 1000 800 600 400
     do
-        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms $Ms --Ax 0.5e-6 --Ku 0.0 --dtime 5.0e-13 --max_iter 200000
+        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms $Ms --Ax 0.5e-6 --Ku 0.0 --dtime 5.0e-13 --max_iter 200000 --loss_type 'divergence' --model_name 'divergence_loss.pt'
     done
 
     for Ku in 1e5 2e5 3e5 4e5
     do
-        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax 0.5e-6 --Ku $Ku --Kvec 1,0,0 --dtime 5.0e-13 --max_iter 200000
+        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax 0.5e-6 --Ku $Ku --Kvec 1,0,0 --dtime 5.0e-13 --max_iter 200000 --loss_type 'divergence' --model_name 'divergence_loss.pt'
     done
 
     for Ax in 0.7e-6 0.6e-6 0.4e-6 0.3e-6
     do
-        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax $Ax --Ku 0.0 --dtime 5.0e-13 --max_iter 200000
+        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax $Ax --Ku 0.0 --dtime 5.0e-13 --max_iter 200000 --loss_type 'divergence' --model_name 'divergence_loss.pt'
     done
 
     for mask in True triangle hole
     do
-        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax 0.5e-6 --Ku 0.0 --dtime 5.0e-13 --max_iter 200000 --mask $mask
+        python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax 0.5e-6 --Ku 0.0 --dtime 5.0e-13 --max_iter 200000 --mask $mask --loss_type 'divergence' --model_name 'divergence_loss.pt'
     done
 
 done
