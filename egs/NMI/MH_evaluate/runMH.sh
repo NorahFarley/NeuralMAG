46e1ce06fcd297d3b534dc747a9a1e024da4904d
NorahFarley <furbynorah@gmail.com>
2026-07-06 05:22:00 -0400

    Debugging

--- egs/NMI/MH_evaluate/runMH.sh
+++ egs/NMI/MH_evaluate/runMH.sh
@@ -22,11 +22,12 @@ do
     for Ku in 1e5 2e5 3e5 4e5
     do
         python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax 0.5e-6 --Ku $Ku --Kvec 1,0,0 --dtime 2.0e-13 --loss_type $loss_type --model_name $model
-
+    done
+    
     for Ax in 0.7e-6 0.6e-6 0.4e-6 0.3e-6
     do
         python ./MH_unet_mm.py --gpu $gpu --w $width --layer 2 --Ms 1000 --Ax $Ax --Ku 0.0 --dtime 2.0e-13 --loss_type $loss_type --model_name $model
-
+    done
 done
 
 
