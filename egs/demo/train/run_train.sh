bfd903763cfb5b28fcbd97d4acc54a2db4acc08d
NorahFarley <furbynorah@gmail.com>
2026-07-24 20:08:40 -0400

    merge conflict

--- egs/demo/train/run_train.sh
+++ egs/demo/train/run_train.sh
@@ -3,14 +3,14 @@
 
 
 
-python ./train.py --batch-size 16 \
-                --lr 0.002 \
+python ./train.py --batch-size 100 \
+                --lr 0.005 \
                 --epochs 1000 \
                 --kc 16 \
                 --inch 6 \
                 --ntrain 301 \
                 --ntest 20 \
                 --gpu 0 \
-                --ex 1.0 \
+                --ex 3.0 \
                 --alpha 0.5 \
                 --loss_type "baseline"
